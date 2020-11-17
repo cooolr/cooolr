c1e3cde2e4a70b21c92ea0dee8af6e40d730f565
liyr <i@lr.cool>
2020-11-17 17:45:30 +0800

    update

--- shell/qpython+.sh
+++ shell/qpython+.sh
@@ -57,7 +57,7 @@ judge "移动dropbear命令"
 apt remove -y dropbear
 judge "卸载dropbear"
 
-echo '''import os
+echo """import os
 if not os.popen("netstat -lnt|grep 8122").read():
     os.system("~/.dropbear -p 8122&&termux-wake-lock")''' >~/.dropbear/runbear.py
 judge "写入python程序，自启动dropbear服务"
@@ -72,29 +72,20 @@ unzip androidhelper.zip -d ~/../usr/lib/python$v/site-packages/androidhelper
 judge "下载安装androidhelper"
 
 echo '''# -*- coding: utf-8 -*-
-
 import os,sys
-
 if sys.version[0] == '2':
     print('\n此脚本仅运行在python3环境, 请安装最新版QPython并切换到python3环境')
     sys.exit()
-
 runfile = os.environ['HOME'] + '/bin/qpython3-android5.sh'
-
 if 'termux' in runfile:
     print('\n你已在termux的python环境中')
     sys.exit()
-
 with open(runfile, 'r') as f:
     text = f.read()
-
 text = text.replace('. $DIR/init.sh && $DIR/python3-android5 "$@"', 'echo $AP_PORT > /sdcard/qpython/AP_PORT&&echo $AP_HANDSHAKE > /sdcard/qpython/AP_HANDSHAKE&&$DIR/ssh -p8122 -i /sdcard/qpython/id_rsa -t localhost "cd /sdcard/qpython&&python $@"')
-
-
 with open(runfile, 'w') as f:
     f.write(text)
-
-print('\n配置完成，编辑器运行环境已切换到termux, 来回切换python版本即可还原')''' >/sdcard/qpython/scripts3/qpython配置termux运行.py
+print('\n配置完成，编辑器运行环境已切换到termux, 来回切换python版本即可还原')""" >/sdcard/qpython/scripts3/qpython配置termux运行.py
 judge "/sdcard/qpython/scripts3/qpython配置termux运行.py"
 
-judge '请在qpython执行 "qpython配置termux运行.py" 即可配置成功'
+echo "${Green}请在qpython执行 <qpython配置termux运行.py> 即可配置成功${Font}"
