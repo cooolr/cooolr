07a5b3a2c90b0ca4ec71c09d77aec9e2936532b3
liyr <i@lr.cool>
2020-11-18 09:29:12 +0800

    add ptpi and repo

---
+++ shell/repo.sh
@@ -0,0 +1,9 @@
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
+pkg update
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
+echo -e "\n"
