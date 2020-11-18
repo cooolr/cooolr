4293b1fbf13c7b6bfc26570695abec62520dec26
liyr <i@lr.cool>
2020-11-18 10:08:28 +0800

    add ptpi and repo

--- shell/pypi.sh
+++ shell/pypi.sh
@@ -1,3 +1,2 @@
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-echo -e "\033[32m[OK]\033[0m \033[42;37m 恭喜！已切换PyPI清华源  \033[0m"
