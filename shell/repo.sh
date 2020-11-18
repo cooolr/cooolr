123698a56f44e5f1f6eacab34f27ac418b7fa569
liyr <i@lr.cool>
2020-11-18 10:12:47 +0800

    add ptpi and repo

--- shell/repo.sh
+++ shell/repo.sh
@@ -1,6 +1,6 @@
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.ustc.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
 
-echo -e "\033[32m[OK]\033[0m \033[42;37m 进入更新程序，请一路回车完成后续步骤 \033[0m"
+echo -e "\033[32m[OK]\033[0m \033[42;37m 修改sources.list成功, 进入更新程序，请一路回车完成后续步骤 \033[0m"
 
 pkg update
 
